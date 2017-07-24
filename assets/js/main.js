461f224ea7903243d627a95e59462941a0dd6de4
romina <romina.torres.b@gmail.com>
2017-07-24 14:21:50 -0400

    seccion 2 - html, sass, js

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+ $(document).ready(function(){
+      $('.carousel').slider();
+    });
+        
+ $('.btn-dropdown').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false,
+      hover: true,
+      gutter: 0,
+      belowOrigin: true,
+      alignment: 'left',
+      stopPropagation: false
+    }
+  );
